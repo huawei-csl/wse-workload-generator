6793b0c26459e414abfb593b57a669e29512cc6b
Ahmet Yuzuguler <ahmet.yuzuguler@huawei.com>
2025-07-25 17:53:23 +0200

    User can now specify which nodes to simulate

--- main.py
+++ main.py
@@ -12,6 +12,20 @@ from generator import Generator
 import logging
 from logger import init_logger
 
+def nodes_to_simulate(nodes, max_num_nodes):
+    # Convert args.nodes to a list of integers
+    if args.nodes == "all":
+        return list(range(max_num_nodes))
+    else:
+        nodes = []
+        for part in args.nodes.split(","):
+            if "-" in part:
+                start, end = map(int, part.split("-"))
+                nodes.extend(range(start, end + 1))
+            else:
+                nodes.append(int(part))
+    return nodes
+
 if __name__=="__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--model_config", type=str, default="configs/deepseekv3.json", help="HF json file path for model config")
@@ -21,8 +35,9 @@ if __name__=="__main__":
     argparser.add_argument("--decode_len", type=int, default=10, help="decode length")
     argparser.add_argument("--only_decode", type=int, choices=[0,1], default=1, help="1: skips prefill, 0: run both prefill and decode")
     argparser.add_argument("--simplified_decode", type=int, choices=[0,1], default=1, help="0: full decode run, 1: run only first and last decode iterations, rest can be interpolated")
+    argparser.add_argument("--nodes", type=str, default="all", help="nodes to run, e.g., 'all', '0,1,2', '0-3' (for 4 nodes), '0-3,5' (for 5 nodes)")
     argparser.add_argument("--dtype", choices=["fp16", "fp8"], default="fp16", help="numeric precision")
-    argparser.add_argument("--log", choices=["debug", "info"], default="info", help="logging level")
+    argparser.add_argument("--log", choices=["debug", "info", "error"], default="info", help="logging level")
     argparser.add_argument("--outdir", type=str, default="./out", help="directory for generated csv files")
     args = argparser.parse_args()
 
@@ -40,14 +55,18 @@ if __name__=="__main__":
     generator = Generator()
     if not args.only_decode:
         prefill_cfg = SystemConfig().from_json(args.system_config, mode="prefill")
+        nodes = nodes_to_simulate(args.nodes, prefill_cfg.num_nodes)
+
         prefill_models = []
-        for rank in range(prefill_cfg.num_nodes):
+        for rank in nodes:
             prefill_models.append(build_model(model_config, prefill_cfg.get_dist_info(rank), args.dtype, out_dir))
 
         generator.prefill(prefill_models, args.bsz, args.prefill_len)
 
     decode_cfg = SystemConfig().from_json(args.system_config, mode="decode")
+    nodes = nodes_to_simulate(args.nodes, decode_cfg.num_nodes)
+
     decode_models = []
-    for rank in range(decode_cfg.num_nodes):
+    for rank in nodes:
         decode_models.append(build_model(model_config, decode_cfg.get_dist_info(rank), args.dtype, out_dir))
     generator.decode(decode_models, args.bsz, args.prefill_len, args.decode_len, args.simplified_decode)
